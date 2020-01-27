7c1476004ac977acc17ac67710f71c7dd47d5294
Yuchen Liu <yuchen.liu@acidgreen.com.au>
2020-01-28 09:11:15 +1100

    fix postfix local emails

--- postfix/run.sh
+++ postfix/run.sh
@@ -7,7 +7,7 @@ function add_config_value() {
   local value=${2}
   local config_file=${3:-/etc/postfix/main.cf}
   [ "${key}" == "" ] && echo "ERROR: No key set !!" && exit 1
-  [ "${value}" == "" ] && echo "ERROR: No value set !!" && exit 1
+#  [ "${value}" == "" ] && echo "ERROR: No value set !!" && exit 1
 
   echo "Setting configuration option ${key} with value: ${value}"
  postconf -e "${key} = ${value}"
@@ -18,18 +18,25 @@ function add_config_value() {
 SMTP_PORT="${SMTP_PORT-587}"
 
 #Get the domain from the server host name
-DOMAIN=`echo ${SERVER_HOSTNAME} |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
+#DOMAIN=`echo ${SERVER_HOSTNAME} |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
 
 # Set needed config options
 add_config_value "myhostname" ${SERVER_HOSTNAME}
 add_config_value "mydomain" ${DOMAIN}
-add_config_value "mydestination" '$myhostname'
+add_config_value "mydestination" '$myhostname, localhost.$mydomain, localhost'
 add_config_value "myorigin" '$mydomain'
-[ -n "${SMTP_SERVER}" ] && add_config_value "relayhost" "[${SMTP_SERVER}]:${SMTP_PORT}"
-add_config_value "smtp_use_tls" "yes"
-add_config_value "smtp_sasl_auth_enable" "yes"
-add_config_value "smtp_sasl_password_maps" "hash:/etc/postfix/sasl_passwd"
-add_config_value "smtp_sasl_security_options" "noanonymous"
+add_config_value "mynetworks" ${MYNETWORKS}
+add_config_value "home_mailbox" "Maildir/"
+add_config_value "local_recipient_maps" ""
+add_config_value "luser_relay" "root@${SERVER_HOSTNAME}"
+
+if [ -n "${SMTP_SERVER}" ]; then
+  add_config_value "relayhost" "[${SMTP_SERVER}]:${SMTP_PORT}"
+  add_config_value "smtp_use_tls" "yes"
+  add_config_value "smtp_sasl_auth_enable" "yes"
+  add_config_value "smtp_sasl_password_maps" "hash:/etc/postfix/sasl_passwd"
+  add_config_value "smtp_sasl_security_options" "noanonymous"
+fi
 
 # Create sasl_passwd file with auth credentials
 if [ -n "${SMTP_SERVER}" ] && [ ! -f /etc/postfix/sasl_passwd ]; then
